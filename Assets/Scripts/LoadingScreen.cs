450e63e57924a338dac59dc7e6758a1b4d317043
darciFromVIP <darcifromvip@gmail.com>
2023-09-05 10:48:32 +0200

    Added loading screen wallpapers

--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -8,10 +8,13 @@ public class LoadingScreen : MonoBehaviour
     public GameObject window;
     public TextMeshProUGUI loadingText, percentageProgress;
     public Slider loadingSlider;
+    public Image loadingScreenImage;
+    public List<Sprite> images;
     public void LoadAsyncOperation(string text, AsyncOperation operation)
     {
         window.SetActive(true);
         loadingText.text = text;
+        loadingScreenImage.sprite = images[Random.Range(0, images.Count)];
         StartCoroutine(Loading(operation));
     }
     private IEnumerator Loading(AsyncOperation operation)
