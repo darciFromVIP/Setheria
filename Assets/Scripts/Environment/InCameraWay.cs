2b8f70c7a02e8609fbee1dd6d778505307d1e1dc
darciFromVIP <darcifromvip@gmail.com>
2023-11-12 12:30:30 +0100

    Trees disappearing now

--- Assets/Scripts/Environment/InCameraWay.cs
+++ Assets/Scripts/Environment/InCameraWay.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InCameraWay : MonoBehaviour
 {
+    public GameObject objectToHide;
+    private void Start()
+    {
+        if (objectToHide == null)
+        {
+            objectToHide = gameObject;
+        }
+    }
     public void TurnOff()
     {
-        foreach (var item in GetComponentsInChildren<MeshRenderer>())
+        foreach (var item in objectToHide.GetComponentsInChildren<MeshRenderer>())
         {
             item.enabled = false;
         }
     }
     public void TurnOn()
     {
-        foreach (var item in GetComponentsInChildren<MeshRenderer>())
+        foreach (var item in objectToHide.GetComponentsInChildren<MeshRenderer>())
         {
             item.enabled = true;
         }
