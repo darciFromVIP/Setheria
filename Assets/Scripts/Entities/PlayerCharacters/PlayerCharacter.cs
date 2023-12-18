2bc7f576a8bb7e93e3545b2afe4ac3611337b6be
darciFromVIP <darcifromvip@gmail.com>
2023-12-18 10:31:36 +0100

    s

--- Assets/Scripts/Entities/PlayerCharacters/PlayerCharacter.cs
+++ Assets/Scripts/Entities/PlayerCharacters/PlayerCharacter.cs
@@ -261,7 +261,7 @@ public class PlayerCharacter : Character, LocalPlayerCharacter
     [TargetRpc]
     public void SaveState(NetworkConnection conn, NetworkIdentity player)
     {
-        Debug.Log("Saving " + gameObject.name + " for connection " + connectionToClient.connectionId);
+        Debug.Log("Saving " + gameObject.name + " for connection " + connectionToServer.connectionId);
         var inventory = FindObjectOfType<InventoryManager>(true).GetAllItems();
         List<SaveDataItem> items = new();
         foreach (var item in inventory)
