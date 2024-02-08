bfc62283cbaaf5c2098da905af005ebde2a49017
darciFromVIP <darcifromvip@gmail.com>
2024-02-08 11:50:33 +0100

    test

--- Assets/Scripts/Entities/CanHavePets.cs
+++ Assets/Scripts/Entities/CanHavePets.cs
@@ -10,11 +10,25 @@ public class CanHavePets : NetworkBehaviour
     public void SpawnPet(string name, Vector3 position, float time, float power)
     {
         var pet = Instantiate(entityDatabase.GetEntityByName(name), position, Quaternion.identity);
-        pet.GetComponent<Pet>().StartTimedLife(time, this);
-        pet.GetComponent<CanAttack>().SetPower(power);
         spawnedPets.Add(pet);
         NetworkServer.Spawn(pet.gameObject, gameObject);
+        RpcInitializePet(pet.GetComponent<NetworkIdentity>(), time, power);
     }
+    [ClientRpc]
+    private void RpcInitializePet(NetworkIdentity pet, float time, float power)
+    {
+        StartCoroutine(WaitForServerLoad(pet, time, power));
+    }
+    private IEnumerator WaitForServerLoad(NetworkIdentity pet, float time, float power)
+    {
+        while (!(pet.isClient || pet.isServer))
+        {
+            yield return null;
+        }
+        pet.GetComponent<Pet>().StartTimedLife(time, this);
+        pet.GetComponent<CanAttack>().SetPower(power);
+    }
+
     public void DespawnPet(Entity pet, bool deathAnimation)
     {
         spawnedPets.Remove(pet);
