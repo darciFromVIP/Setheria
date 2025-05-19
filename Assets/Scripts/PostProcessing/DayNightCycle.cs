3abb0ad699c3d00b9a19a847bab3e9a3932cc501
darciFromVIP <darcifromvip@gmail.com>
2025-05-19 14:42:11 +0200

    Update DayNightCycle.cs

--- Assets/Scripts/PostProcessing/DayNightCycle.cs
+++ Assets/Scripts/PostProcessing/DayNightCycle.cs
@@ -48,7 +48,6 @@ public class DayNightCycle : NetworkBehaviour, ISaveable
     private void Start()
     {
         maxIndex = data.Count - 1;
-        daysAlive = 1;
         FindObjectOfType<AudioManager>().SetDayNightCycle(this);
     }
     public bool IsNight()
